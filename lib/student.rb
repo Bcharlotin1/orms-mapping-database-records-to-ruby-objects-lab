f2079ef2da47d1fccb3581bc8187e24cc3ef350a
bcharlotin1 <biancacharlotin1@gmail.com>
2021-02-19 18:44:52 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -43,9 +43,9 @@ class Student
               FROM students
               WHERE grade = 9
           SQL
-          DB[:conn].execute(sql).map do |row|
-                  self.new_from_db(row)
-          end.first
+          # DB[:conn].execute(sql).map do |row|
+          #         self.new_from_db(row)
+          # end.first
   end
   
   def save
